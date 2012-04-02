357c9d3ec443a65cea20970528682da56a3d3e2d
Kush Patel <kushpatel72@gmail.com>
2012-04-02 16:19:04 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grabbag::Application.config.secret_token = 'b8f56419ed0a8148b27119db24560dc21de309e0661ab313079741e70f06788f0e30dc6ee979aa9251c0edd9bea008697e4a747fbf700a04024fffd8d5d783f4'
