a9110192bcb1fe7449e0b5b6974465543a15e555
Kush Patel <kushpatel72@gmail.com>
2012-04-03 22:29:53 -0700

    User model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+    
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  
+  validates_length_of :first_name, :maximum => 50
+  validates_length_of :last_name, :maximum => 50
     
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,31 @@ require 'spec_helper'
 describe User do
   
   before do
-    @user = User.new(:email => "hello@gmail.com", :password => "foobar")
+    @user = User.new(:email => "hello@gmail.com", :password => "foobar", :first_name => "Joe", :last_name => "Blow")
   end
  
+  it { @user.should be_valid }
+  
+  # Devise validates email and password fields
+  
+  describe "when first_name is not present" do
+    before { @user.first_name = " " }
+    it { @user.should_not be_valid }
+  end
+  
+  describe "when last_name is not present" do
+    before { @user.last_name = " " }
+    it { @user.should_not be_valid }
+  end
+  
+  describe "when first_name is too long" do
+    before { @user.first_name = "a" * 51 }
+    it { @user.should_not be_valid }
+  end
+  
+  describe "when last_name is too long" do
+    before { @user.last_name = "a" * 51 }
+    it { @user.should_not be_valid }
+  end
   
 end
